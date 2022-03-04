2bef6fb4b1647bed4c419fbbcf20118c871849b9
Olivier Baumann <olivier@iovi.io>
2022-03-04 20:09:26 +0100

    add favicons

--- components/Layout.js
+++ components/Layout.js
@@ -16,6 +16,79 @@ export default function Layout({ children }) {
           href='https://fonts.googleapis.com/css2?&family=Grandstander:wght@800&family=Signika:wght@500;700&display=swap'
           rel='stylesheet'
         />
+        <link
+          rel='apple-touch-icon'
+          sizes='57x57'
+          href='images/favicon/apple-icon-57x57.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='60x60'
+          href='images/favicon/apple-icon-60x60.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='72x72'
+          href='images/favicon/apple-icon-72x72.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='76x76'
+          href='images/favicon/apple-icon-76x76.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='114x114'
+          href='images/favicon/apple-icon-114x114.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='120x120'
+          href='images/favicon/apple-icon-120x120.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='144x144'
+          href='images/favicon/apple-icon-144x144.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='152x152'
+          href='images/favicon/apple-icon-152x152.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='images/favicon/apple-icon-180x180.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='images/favicon/android-icon-192x192.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='images/favicon/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='96x96'
+          href='images/favicon/favicon-96x96.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='images/favicon/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
+        <meta name='theme-color' content='#ffffff' />
       </Head>
       <Navbar />
       {children}
@@ -37,9 +110,12 @@ export default function Layout({ children }) {
               ? '0% 10%'
               : activeSection === 'info'
               ? '0% 45%'
-              : activeSection === 'signup'
+              : activeSection === 'about'
               ? '0% 70%'
-              : '0% 90%'};
+              : activeSection === 'donate'
+              ? '0% 90%'
+              // Fallback
+              : '0% 10%'};
             transition: background-position 1s ease-in-out;
           }
           /* @media (prefers-color-scheme: dark) {
