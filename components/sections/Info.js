e52c1a433dabeef86c39d1704f7b1d023e29fdc5
Adrian Demleitner <adrian@thgie.ch>
2022-03-21 15:31:19 +0100

    Apply suggestions from code review
    Co-authored-by: Olivier Baumann <olivierbaumann@me.com>

--- components/sections/Info.js
+++ components/sections/Info.js
@@ -160,9 +160,9 @@ const CampInfo = ({ id }) => {
           />
           <p className='mt-4'>
             Das Coding Camp findet im Residency-Haus{' '}
-            <a href='https://cimacitta.ch/de' target='_blank' Icon={ExternalLinkIcon}>
+            <A href='https://cimacitta.ch/de' target='_blank' rel="noreferrer" Icon={ExternalLinkIcon}>
               Cima Citta
-            </a>
+            </A>
             , in Dangio, Tessin statt. Die Cima Citta befindet sich auf dem
             Gelände der ehemaligen Schokoladenfabrik <i>Cima Norma</i>. Sie
             liegt im Blenio Tal und ist in einer halben Stunde von Biasca mit
