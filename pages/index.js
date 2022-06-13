b631058d910b3d169f9f3c36e327d3ce4c51326d
Olivier Baumann <olivier@iovi.io>
2022-06-13 14:57:23 +0200

    fix seo

--- pages/index.js
+++ pages/index.js
@@ -14,21 +14,22 @@ export default function Home({}) {
       <Head>
         <title>{title}</title>
         <meta name='description' content={description} />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content={title} />
-        <meta property='og:description' content={description} />
-        <meta property='og:site_name' content={title} />
+        <meta name='og:type' content='website' />
+        <meta name='og:title' content={title} />
+        <meta name='og:description' content={description} />
+        <meta name='og:site_name' content={title} />
         <meta
-          property='og:image'
+          name='og:image'
           content='https://queercodingcamp.ch/images/queercodingcamp_og_image.png'
         />
-        <meta property='twitter:card' content='summary' />
-        <meta property='twitter:title' content={title} />
+        <meta name='twitter:creator' content='@obauma' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={title} />
         <meta
-          property='twitter:image'
+          name='twitter:image'
           content='https://queercodingcamp.ch/images/queercodingcamp_og_image.png'
         />
-        <meta property='twitter:description' content={description} />
+        <meta name='twitter:description' content={description} />
       </Head>
       <main className='flex flex-col items-center justify-center w-full flex-1 text-center'>
         <TopSection id='top' />
