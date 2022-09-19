866740d2eab4815c15533334dc65461571bee846
Olivier Baumann <olivierbaumann@me.com>
2022-09-19 12:57:55 +0200

    remove signup

--- components/sections/Signup.js
+++ components/sections/Signup.js
@@ -11,6 +11,15 @@ const SignUp = ({ id }) => {
       <div className='flex flex-col md:flex-row gap-12 mt-12'>
         <div className='flex-1 text-left'>
           <h2 className='text-2xl font-bold text-left font-display'>
+            Newsletter
+          </h2>
+          <p className='text-left mb-4'>
+            Die Anmeldung für das Coding Camp im Oktober 2022 ist geschlossen.
+            Willst du Neuigkeiten zum Camp und zukünftige Camps informiert
+            werden? Dann trage dich in unseren Newsletter ein.
+          </p>
+          <NewlsetterSignup />
+          {/* <h2 className='text-2xl font-bold text-left font-display'>
             Letzte Plätze
           </h2>
           <p className='text-left mb-4'>
@@ -20,19 +29,9 @@ const SignUp = ({ id }) => {
               hi@queercodingcamp.ch
             </Link>
             .
-          </p>
+          </p> */}
         </div>
-        <div className='flex-1 text-left'>
-          <h2 className='text-2xl font-bold text-left font-display'>
-            Newsletter
-          </h2>
-          <p className='text-left mb-4'>
-            Die Anmeldung für das Coding Camp im Oktober 2022 ist geschlossen.
-            Willst du Neuigkeiten zum Camp und zukünftige Camps informiert
-            werden? Dann trage dich in unseren Newsletter ein.
-          </p>
-          <NewlsetterSignup />
-        </div>
+        <div className='flex-1 text-left'></div>
       </div>
     </SectionContainer>
   )
