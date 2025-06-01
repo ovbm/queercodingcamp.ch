e3c0897eecb8afe8443598985f852e2fee1b5d92
Olivier Baumann <olivierbaumann@me.com>
2025-06-01 09:31:16 +0200

    fix sitemap, remove unused config file

--- config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title: 'Queer Coding Camp',
-  author: {
-    name: 'Olivier Baumann',
-    mail: 'olivier@iovi.io',
-    twitter: '@obauma',
-  },
-  description: 'Programmieren lernen für junge queere Menschen.',
-}
